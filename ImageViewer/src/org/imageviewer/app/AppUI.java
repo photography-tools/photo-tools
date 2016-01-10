6d8b1a2d71fdfc698bcbbd6664238a7283f76f18
seandinwiddie <seandinwiddie@gmail.com>
2016-01-09 18:40:20 -0800

    Added functionality per image maximization and thumb-nail scaling.

--- ImageViewer/src/org/imageviewer/app/AppUI.java
+++ ImageViewer/src/org/imageviewer/app/AppUI.java
@@ -21,44 +21,55 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.border.BevelBorder;
+import javax.swing.JSlider;
+import javax.swing.JScrollPane;
 
 import org.imageviewer.api.IAppServices;
 import org.imageviewer.api.IAppUI;
+import org.imageviewer.app.ImagePanel;
 
-public class AppUI implements IAppUI {
+public class AppUI implements IAppUI{
 
 	private JFrame frame;
-	private ImagePanel centerPanel;
+	private JScrollPane scrollPane;
+	private ImagePanel imagePanel;
+	private JSlider slider;
+	private JPanel statusPanel;
 	private JLabel statusLabel;
 	private IAppServices services;
 
-	public AppUI() {
-		frame = new JFrame();
-		centerPanel = new ImagePanel();
-		statusLabel = new JLabel("Application started. Use File->Open to select a working folder.");
+	public AppUI(){
+		frame=new JFrame();
+		imagePanel=new ImagePanel();
+		scrollPane=new JScrollPane(imagePanel,
+	            JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+	            JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);	
+		slider=new JSlider(0,100,0);
+        slider.setMajorTickSpacing(25);
+        slider.setPaintTicks(true);
+        slider.setSnapToTicks(true);
+        slider.setPaintLabels(true);
+		statusPanel=new JPanel();
+		statusLabel=new JLabel("Application started. Use File->Open to select a working folder.");
 	}
 
 	@Override
-	public void start(IAppServices svc) {
+	public void start(IAppServices svc){
 		
-		this.services = svc;
+		this.services=svc;
 
 		addMenu(frame);
 		addStatus(frame.getContentPane());
 
-		frame.setState(Frame.MAXIMIZED_BOTH);
 		frame.setExtendedState(Frame.MAXIMIZED_BOTH);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		centerPanel.setPreferredSize(new Dimension(frame.getWidth(), frame.getHeight()));
-		frame.getContentPane().add(centerPanel, BorderLayout.CENTER);
-
 		frame.setVisible(true);
-	}
+		scrollPane.setPreferredSize(new Dimension(frame.getWidth(), frame.getHeight() - 128));
+		frame.add(scrollPane, BorderLayout.NORTH);		
+		frame.add(slider, BorderLayout.CENTER);		
+    }
 
-	private void addStatus(Container cpane) {
-		JPanel statusPanel = new JPanel();
-
+	private void addStatus(Container cpane){
 		statusPanel.setBorder(new BevelBorder(BevelBorder.LOWERED));
 		statusPanel.setPreferredSize(new Dimension(cpane.getWidth(), 16));
 		statusPanel.setLayout(new BoxLayout(statusPanel, BoxLayout.X_AXIS));
@@ -67,19 +78,19 @@ public class AppUI implements IAppUI {
 		cpane.add(statusPanel, BorderLayout.SOUTH);
 	}
 
-	private void addMenu(JFrame frame) {
-		JMenuBar menubar = new JMenuBar();
-		ImageIcon icon = new ImageIcon("exit.png");
+	private void addMenu(JFrame frame){
+		JMenuBar menubar=new JMenuBar();
+		ImageIcon icon=new ImageIcon("exit.png");
 
-		JMenu file = new JMenu("File");
+		JMenu file=new JMenu("File");
 		file.setMnemonic(KeyEvent.VK_F);
 
-		JMenuItem eMenuItem = new JMenuItem("Exit", icon);
+		JMenuItem eMenuItem=new JMenuItem("Exit", icon);
 		eMenuItem.setMnemonic(KeyEvent.VK_E);
 		eMenuItem.setToolTipText("Exit application");
 		eMenuItem.addActionListener(event -> System.exit(0));
 
-		JMenuItem openItem = new JMenuItem("Open");
+		JMenuItem openItem=new JMenuItem("Open Folder");
 		openItem.setMnemonic(KeyEvent.VK_O);
 		openItem.addActionListener(event -> this.actionOpen(event));
 
@@ -90,26 +101,34 @@ public class AppUI implements IAppUI {
 		frame.setJMenuBar(menubar);
 	}
 
-	private void actionOpen(ActionEvent event) {
-		JFileChooser fc = new JFileChooser();
+	private void actionOpen(ActionEvent event){
+		JFileChooser fc=new JFileChooser();
 		fc.setCurrentDirectory(new java.io.File(".")); // start at application
 														// current directory
 		fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-		int returnVal = fc.showSaveDialog(this.frame);
-		if (returnVal == JFileChooser.APPROVE_OPTION) {
+		fc.setDialogTitle("Select Folder");
+//		fc.setApproveButtonText("Select");
+//		System.out.println(fc.getApproveButtonText());
+		fc.setApproveButtonToolTipText("Open images in this folder");
+		int returnVal=fc.showSaveDialog(this.frame);
+		if (returnVal == JFileChooser.APPROVE_OPTION){
 			services.onOpen(fc.getSelectedFile());
 		}
 	}
 
 	@Override
-	public void setStatus(String status) {
+	public void setStatus(String status){
 		this.statusLabel.setText(status);
 	}
 
 	@Override
-	public void setWorkingImages(List<File> images) throws IOException {
-		centerPanel.populate(images);
-		centerPanel.revalidate();
+	public void setWorkingImages(List<File> images) throws IOException{
+		int size=slider.getValue()==100?5
+			:slider.getValue()>=75?6
+			:slider.getValue()>=50?7
+			:slider.getValue()>=25?8
+			:9;
+		imagePanel.populate(images,size,slider);
+		imagePanel.revalidate();
 	}
-
 }
