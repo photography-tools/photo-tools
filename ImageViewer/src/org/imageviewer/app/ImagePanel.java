98b76dbb5db3a874bb4591a3e16339e284cc7f50
seandinwiddie <seandinwiddie@gmail.com>
2016-01-09 18:39:22 -0800

    Added functionality per image maximization and thumb-nail scaling.

--- ImageViewer/src/org/imageviewer/app/ImagePanel.java
+++ ImageViewer/src/org/imageviewer/app/ImagePanel.java
@@ -1,5 +1,8 @@
 package org.imageviewer.app;
-import java.awt.GridLayout;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.Component;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -9,44 +12,106 @@ import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import org.imageviewer.util.ImageResize;
+import org.imageviewer.util.WrapLayout;
 
 /*
- * @author akhilkarun
+ * original @author akhilkarun
+ * heavily modified*
  *
  */
 
 @SuppressWarnings("serial")
-public class ImagePanel extends JPanel {
+public class ImagePanel extends JPanel{
 	
-	public void populate(List<File> images) throws IOException {
-		
-		GridLayout gridLayout = new GridLayout(2, 2);
-		
-		images.stream().forEach(file -> {
-			BufferedImage img = readFile(file);
-			int height = this.getHeight() / 2;
-			int width = this.getWidth() / 2;
+	int height;
+	int width;
+	boolean click=true;
+	Component[] components;
+	ImagePanel container;
+	ImagePanel image;
+	
+	public void populate(List<File> images,int sizeMod,JSlider slider) throws IOException{
+		//sizeMod=(int)Math.round(sizeMod*0.5);
+		images.stream().forEach(file ->{
+			image=this;
+			BufferedImage img=readFile(file);
+			height=image.getHeight() / sizeMod;
+			width=image.getWidth() / sizeMod;
+			width=(int) Math.min(width, height *(img.getWidth() * 1.0 / img.getHeight()));
 			
-			// crunch horizontally to maintain aspect ratio
-			width = (int) Math.min(width, height * (img.getWidth() * 1.0 / img.getHeight()));
+			ImageIcon icon=new ImageIcon(ImageResize.resizeImageWithHint(img,height,width));
+			JLabel label=new JLabel(icon);
 			
-			ImageIcon icon = new ImageIcon(ImageResize.resizeImageWithHint(img,height, width));
-			JLabel label = new JLabel(icon);
+			label.addMouseListener(new MouseAdapter(){
+		      public void mouseClicked(MouseEvent e){
+		    	if(e.getClickCount()==2&&!e.isConsumed()&&click){
+		    		e.consume();
+		    		label.setText(label.getName());
+		    		height=container.getHeight();
+		    		width=container.getWidth();
+		    		width=(int) Math.min(width, height *(img.getWidth() * 1.0 / img.getHeight()));
+		    		icon.setImage(ImageResize.resizeImageWithHint(img,height,width));
+		    		click=!click;
+		    		for(int i=0;i<components.length;i++){
+		    	        if(components[i]==label){
+		    	        	components[i].setVisible(true);
+		    	        }
+		    	        else{
+		    	        	components[i].setVisible(false);
+		    	        }
+		    	    }
+		    		
+		      	}
+		    	if(e.getClickCount()==2&&!e.isConsumed()&&!click){
+		    		e.consume();	    				    	  
+		    		label.setText("");
+		    		int size=slider.getValue()==100?5
+	    				:slider.getValue()>=75?6
+						:slider.getValue()>=50?7
+						:slider.getValue()>=25?8
+						:9;
+		    		height=image.getHeight() / size;
+		    		width=image.getWidth() / size;
+		    		width=(int) Math.min(width, height *(img.getWidth() * 1.0 / img.getHeight()));
+		    		icon.setImage(ImageResize.resizeImageWithHint(img,height,width));
+		    		click=!click;
+		    		for(int i=0;i<components.length;i++){components[i].setVisible(true);}
+		      	}
+		      }
+		    });
+			slider.addChangeListener(new ChangeListener() {
+			      public void stateChanged(ChangeEvent event) {
+			    	  int size=slider.getValue()==100?5
+						:slider.getValue()>=75?6
+						:slider.getValue()>=50?7
+						:slider.getValue()>=25?8
+						:9;
+					  height=image.getHeight() / size;
+					  width=image.getWidth() / size;
+					  width=(int) Math.min(width, height *(img.getWidth() * 1.0 / img.getHeight()));
+					  icon.setImage(ImageResize.resizeImageWithHint(img,height,width));
+					  container.revalidate();
+		    	  }
+		    });
+
 			this.add(label);
+			components=this.getComponents();
 		});
 
-		this.setLayout(gridLayout);
-
+		this.setLayout(new WrapLayout());
+		container=this;
 	}
 
-	private BufferedImage readFile(File file) {
-		try {
+	private BufferedImage readFile(File file){
+		try{
 			return ImageIO.read(file);
-		} catch (IOException e) {
+		}catch(IOException e){
 			throw new RuntimeException(e);
 		}
 	}
-
 }
